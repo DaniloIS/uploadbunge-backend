cb439c0bee8d800d044fc3b6ca5648c9d000b420
DaniloIS <danilosantos-pg@hotmail.com>
2020-07-23 00:06:33 -0300

    add config postgres

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
-import dotenv from 'dotenv';
+
 import path from 'path';
 
-dotenv.config();
 
 /*module.exports = {
     client: 'sqlite3',
@@ -17,7 +16,7 @@ dotenv.config();
 module.exports = {
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL
+        connectionString: 'postgres://usdnmrezutwscu:24671409b9402d6c28e071d0a00a6127b4d8562e7651970981430474efa14478@ec2-52-200-48-116.compute-1.amazonaws.com:5432/d2c1b2bp8h7oum'
     },
     migrations: {
         directory: path.resolve(__dirname, 'dist', 'src', 'database', 'migrations')
